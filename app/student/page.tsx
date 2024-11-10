887d0219ca0124eddea482c7dfa46ba78fc7f542
thannichasompan <88131261+thannichasompan@users.noreply.github.com>
2024-11-11 00:01:57 +0700

    edit student

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -4,7 +4,7 @@ import MemoButton from '@/components/button/memo-button';
 import MemoCard from '@/components/container/memo-card';
 import MemoInputText from '@/components/input/memo-input-text';
 import MultiStep from '@/components/step/multi-step';
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -20,52 +20,52 @@ export default function StudentRegistrationForm() {
     <BrandingBackground>
       <MemoCard>
       <MultiStep step={step} steps={["สร้างบัญชีนักเรียน", "ข้อมูลนักเรียน", "ข้อมูลผู้ปกครอง"]}/>
-      {step === 1 && (
-        <section className="flex flex-col items-center space-y-xl">
-          <Image src="/Logo-circle.png" alt="logo.png" width={140} height={140} />
-          <p className="text-body-1 text-header font-bold">ส่งคำร้องเข้าใช้ระบบนักเรียน</p>
-        </section>
-      )}
-      {step === 2 && (
-        <section className="flex flex-col items-center space-y-xl">
-          <Image src="/Logo-circle.png" alt="logo.png" width={140} height={140} />
-          <p className="text-body-1 text-header font-bold">กรอกประวัตินักเรียน</p>
-        </section>
-      )}
-      {step === 3 && (
-        <section className="flex flex-col items-center space-y-xl">
-          <Image src="/Logo-circle.png" alt="logo.png" width={140} height={140} />
-          <p className="text-body-1 text-header font-bold">กรอกประวัติผู้ปกครอง</p>
-        </section>
-      )}
         <form className="flex flex-col space-y-lg" onSubmit={(e) => e.preventDefault()}>
           {step === 1 && (
-            <div className="flex flex-col space-y-lg">
-              <MemoInputText placeholder="รหัสประจำตัวนักเรียน" />
-              <MemoInputText placeholder="อีเมล" />
-              <MemoInputText placeholder="รหัสผ่าน" />
-              <MemoInputText placeholder="ยืนยันรหัสผ่าน" /> 
-            </div>
+            <Fragment>
+              <section className="flex flex-col items-center space-y-xl">
+                <Image src="/Logo-circle.png" alt="logo.png" width={140} height={140} />
+                <p className="text-body-1 text-header font-bold ">ส่งคำร้องเข้าใช้ระบบนักเรียน</p>
+              </section>
+              <div className="flex flex-col space-y-lg">
+                <MemoInputText placeholder="รหัสประจำตัวนักเรียน" />
+                <MemoInputText placeholder="อีเมล" />
+                <MemoInputText placeholder="รหัสผ่าน" />
+                <MemoInputText placeholder="ยืนยันรหัสผ่าน" /> 
+              </div>
+            </Fragment>
           )}
           {step === 2 && (
-            <div className="flex flex-col space-y-lg">
-              <MemoInputText placeholder="ชื่อนักเรียน" />
-              <MemoInputText placeholder="นามสกุลนักเรียน" />
-              <MemoInputText placeholder="ชั้นเรียน" />
-              <MemoInputText placeholder="ยืนยันรหัสผ่าน" /> 
-            </div>
+            <Fragment>
+              <section className="flex flex-col items-center space-y-xl">
+                <Image src="/Logo-circle.png" alt="logo.png" width={140} height={140} />
+                <p className="text-body-1 text-header font-bold">กรอกประวัตินักเรียน</p>
+              </section>
+              <div className="flex flex-col space-y-lg">
+                <MemoInputText placeholder="ชื่อนักเรียน" />
+                <MemoInputText placeholder="นามสกุลนักเรียน" />
+                <MemoInputText placeholder="ชั้นเรียน" />
+                <MemoInputText placeholder="ยืนยันรหัสผ่าน" /> 
+              </div>
+            </Fragment>
           )}
           {step === 3 && (
-            <div className="flex flex-col space-y-lg">
-              <MemoInputText placeholder="ชื่อผู้ปกครอง" />
-              <MemoInputText placeholder="นามสกุลผู้ปกครอง" />
-              <MemoInputText placeholder="เบอร์ผู้ปกครอง" />
-              <MemoInputText placeholder="ความสัมพันธ์" /> 
-            </div>
+            <Fragment>
+              <section className="flex flex-col items-center space-y-xl">
+                <Image src="/Logo-circle.png" alt="logo.png" width={140} height={140} />
+                <p className="text-body-1 text-header font-bold">กรอกประวัติผู้ปกครอง</p>
+              </section>
+              <div className="flex flex-col space-y-xl">
+                <MemoInputText type="text" placeholder="ชื่อผู้ปกครอง" />
+                <MemoInputText type="text" placeholder="นามสกุลผู้ปกครอง" />
+                <MemoInputText type="number" placeholder="เบอร์ผู้ปกครอง" />
+                <MemoInputText type="text" placeholder="ความสัมพันธ์" /> 
+              </div>
+            </Fragment>
           )}
           
-          <MemoButton title="ถัดไป" />
-          <button onClick={handleNext}>ถัดไป</button>
+          <MemoButton onClick={handleNext} title="ถัดไป" />
+      
           <Link href="/">
                 <MemoButton title="กลับไปยังหน้าเลือกผู้ใช้" variant="ghost"/>
           </Link>
